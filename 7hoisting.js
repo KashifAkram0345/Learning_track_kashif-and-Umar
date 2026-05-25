d6334a397b7a29a3a118e135d61118266a413bc9
KashifAkram0345 <m.kashif6833@gmail.com>
2026-05-26 02:13:06 +0500

    Question 7

---
+++ 7hoisting.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+// function hoisting possible only plain fuction
+// greet()
+
+// let greet=function(){
+//    console.log("Kashif") 
+// }
+// // function hoisting 
+// sayName("Kashif Akram")
+// function sayName(finalName){
+//     console.log(finalName)
+// }
+// function sayName(){
+//     console.log("Kashif")
+// }
+// sayName()
