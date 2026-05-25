8810ebebc90b7dd18bd925883db9bec9c1128ef2
KashifAkram0345 <m.kashif6833@gmail.com>
2026-05-26 02:08:36 +0500

    Question 1

---
+++ 1index.js
@@ -0,0 +1,27 @@
+//  console.log("Hell Umar Bahi")
+//  var x=10; 
+//  console.log(x);
+//  function solve(){
+//     var x=10;
+//     console.log(x)
+//  }
+//  solve();
+
+let x=10;
+console.log(x)
+if(1){
+    let x=20
+    console.log(x)
+}
+const y=30;
+console.log(y);
+
+
+function marks(){
+    let mark=100;
+    console.log(mark)
+}
+let boolean=true;
+console.log(boolean);
+let strings="kashif"
+console.log(strings)
