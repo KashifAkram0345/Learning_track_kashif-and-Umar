9e28ca718ae610a2b8b9351837024254b60f697d
KashifAkram0345 <m.kashif6833@gmail.com>
2026-05-26 02:09:53 +0500

    Question 3

---
+++ 3index.js
@@ -0,0 +1,23 @@
+// let a=2;
+// let c=3;
+// console.log(a+c);
+ 
+// let z=3;
+
+// console.log(++z)
+
+// console.log(5==5);
+
+
+// let ans = (false&&true&&false)
+// console.log(ans)
+// let ans1=(false||true||false)
+// console.log(ans1);
+// let ans2=!(true)
+// console.log(ans2)
+
+
+ //1 0&&
+console.log(3&5)
+console.log(3|5)
+console.log(2^2)
