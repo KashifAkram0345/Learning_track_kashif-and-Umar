4affd52d68c0a4f185dfd656500e4c11b2493810
KashifAkram0345 <m.kashif6833@gmail.com>
2026-05-26 02:11:05 +0500

    Question 5

---
+++ 5function.js
@@ -0,0 +1,33 @@
+// console.log("Hy How are you kashif")
+// function solve(){
+//     console.log("Function inside ");
+// }
+// solve();
+
+// function Count(){
+//     for(let i=1;i<100;i++){
+//         console.log(i)
+//     }
+// }
+// Count();
+
+// function getprint(num){
+// console.log("printing the number",num)
+
+// }
+// getprint(10);
+// function getAvg(num1,num2){
+//     let avg=(num1+num2)/2
+//    console.log(avg)
+
+// }
+// getAvg(3,70);
+let getMultiplication = function(a, b) {
+    return a * b;
+}
+
+let ans = getMultiplication(2, 20);
+
+console.log(ans);
+
+
