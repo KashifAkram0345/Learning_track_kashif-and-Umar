be8d6ce76f9a0ef829180b38645e1393010da0fb
KashifAkram0345 <m.kashif6833@gmail.com>
2026-05-26 02:10:17 +0500

    Question 4

---
+++ 4loop.js
@@ -0,0 +1,25 @@
+// for(let i=1;i<=10;i++){
+//     console.log("hy! Kashif how are how")
+// }
+// let i=1;
+// while(i<=100){
+ 
+//     console.log(i)
+// i++;
+ 
+// }
+
+// let FirstName='Kashif';
+// let LastName='Akram';
+// let ans =FirstName+LastName;
+// console.log(ans)
+let op1="English";
+let op2="Urdu";
+let ans= op1 + op2;
+console.log(ans)
+let str="please talk in english";
+console.log(str.length)
+let str1="hy umar how are you"
+console.log(str1.toUpperCase())
+let str2="GOOD AFTER NOON"
+console.log(str2.toLowerCase());
