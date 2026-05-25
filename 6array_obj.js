17f22bdf089ddbf4126bee868b33096c1877ffa1
KashifAkram0345 <m.kashif6833@gmail.com>
2026-05-26 02:12:39 +0500

    Question 6

---
+++ 6array_obj.js
@@ -0,0 +1,25 @@
+// let obj={
+//     name:"kashif",
+//     age:20,
+//     weight:50,
+
+// };
+
+// let arr=[1,2,3,4,5]
+
+// let arr1=['Kashif',true,1,2]
+// console.log(arr1)
+// arr1.pop("Umar");
+// arr1.push()
+// arr1.shift();
+// arr1.unshift("umar")
+// let arr3=[2,3,4,4,2,60,61,78,46,2,13]
+// arr3.sort()
+
+// console.log(arr1)
+
+function showArray(arr) {
+  console.log(arr);
+}
+
+showArray([1, 2, 3]);
